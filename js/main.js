1bce4d64a1d53e257b8b02e41aa9ceeea0dafb11
Yukiji Nakatsuka <lunarbase@outlook.com>
2018-06-20 22:47:06 -0700

    Js updated

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// input toro to HTML h1
+var sushi = 'toro';
+document.getElementById('myH1').innerHTML= sushi;
+
+//input & button function
+var input = document.getElementById('myInput');
+function myFunction() {
+  console.log(myInput.value);
+}
+
+var input = document.getElementById('myName');
+function myFunction() {
+  myName.innerText = myInput.value;
+}
+
+var person = [
+{
+  name: Sarah,
+  age: 21,
+  location: CA
+},
+{
+  name: Tomas,
+  age: 18,
+  location: OR
+},
+{
+  name: Peter,
+  age> 31,
+  location FL
+}
+];
